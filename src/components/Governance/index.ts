7496c6907fdf259dec79929c2ff027e56d38904f
ahmedeagle <ahmed.eagle@gmail.com>
2026-01-03 12:00:00 +0900

    Update docs in index.ts

---
+++ src/components/Governance/index.ts
@@ -0,0 +1,3 @@
+export { default as Governance } from './Governance';
+export { default as ClinicianFeedback } from './ClinicianFeedback';
+export { default as GovernanceDashboards } from './GovernanceDashboards';
